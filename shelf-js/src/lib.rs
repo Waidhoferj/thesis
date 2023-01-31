a76b7621bb4780aecd47c8de9287979322699eec
waidhoferj <waidhoferj@gmail.com>
2023-01-31 09:09:08 -0800

    updated js bindings

--- shelf-js/src/lib.rs
+++ shelf-js/src/lib.rs
@@ -47,7 +47,7 @@ impl Fuzzer {
     }
 
     #[wasm_bindgen(js_name = "generateContent")]
-    pub fn generate_shelf_content(&mut self) -> Result<JsValue, JsValue> {
+    pub fn generate_content(&mut self) -> Result<JsValue, JsValue> {
         JsValue::from_serde(&self.0.generate_json_values())
             .or_else(|_| Err(JsValue::from("Failed to convert shelf to JSON")))
     }
@@ -103,190 +103,188 @@ pub struct Awareness {
     users: HashMap<usize, ShelfCRDT>,
 }
 
-#[wasm_bindgen]
-impl Awareness {
-    #[wasm_bindgen(constructor)]
-    pub fn new(uid: Option<usize>) -> Self {
-        let uid = uid.unwrap_or_else(|| rand::thread_rng().gen()); // TODO: Random gen
-        let mut users = HashMap::new();
-        users.insert(uid.clone(), ShelfCRDT::default());
+// #[wasm_bindgen]
+// impl Awareness {
+//     #[wasm_bindgen(constructor)]
+//     pub fn new(uid: Option<usize>) -> Self {
+//         let uid = uid.unwrap_or_else(|| rand::thread_rng().gen());
+//         let mut users = HashMap::new();
+//         Awareness { uid, users }
+//     }
 
-        Awareness { uid, users }
-    }
+//     #[wasm_bindgen(js_name = "getUsers")]
+//     pub fn get_users(&self) -> js_sys::Object {
+//         let object = js_sys::Object::new();
+//         for (user, shelf) in self.users.iter() {
+//             let values = shelf.to_json_values();
+//             js_sys::Reflect::set(&object, &JsValue::from(user), &JsValue::from(values)).unwrap();
+//         }
+//         return object;
+//     }
+//     // awareness.getUser(0).get("foo", "bar")
+//     // awareness.getUser(0).get("foo", "bar")
+//     // awareness.getUser(0).set(["foo", "bar"], 2)
+//     // awareness.getUser(0).foo.bar
+//     // awareness.user(0).get("foo", "bar").value
+//     // awareness.user(0).set("foo", "bar").value = 7
+//     #[wasm_bindgen(js_name = "getUser")]
+//     pub fn get_user(&self, uid: usize) -> Option<ShelfView> {
+//         self.users
+//             .get(&uid)
+//             .map(|shelf| ShelfView::new(Rc::clone(shelf)))
+//     }
+//     #[wasm_bindgen(method, getter)]
+//     pub fn state(&self) -> Option<ShelfView> {
+//         self.get_user(self.uid)
+//     }
+//     #[wasm_bindgen(method, setter)]
+//     pub fn set_state(&mut self, val: &JsValue) {
+//         let json = val.into_serde().unwrap();
+//         let shelf = ShelfCRDT::from_json_values(json, 0).unwrap();
+//         self.users.get_mut(&self.uid).unwrap().replace(shelf);
+//     }
 
-    #[wasm_bindgen(js_name = "getUsers")]
-    pub fn get_users(&self) -> js_sys::Object {
-        let object = js_sys::Object::new();
-        for (user, shelf) in self.users.iter() {
-            let values = shelf.to_json_values();
-            js_sys::Reflect::set(&object, &JsValue::from(user), &JsValue::from(values)).unwrap();
-        }
-        return object;
-    }
-    // awareness.getUser(0).get("foo", "bar")
-    // awareness.getUser(0).get("foo", "bar")
-    // awareness.getUser(0).set(["foo", "bar"], 2)
-    // awareness.getUser(0).foo.bar
-    // awareness.user(0).get("foo", "bar").value
-    // awareness.user(0).set("foo", "bar").value = 7
-    #[wasm_bindgen(js_name = "getUser")]
-    pub fn get_user(&self, uid: usize) -> Option<ShelfView> {
-        self.users
-            .get(uid)
-            .map(|shelf| ShelfView::new(Rc::clone(shelf)))
-    }
-    #[wasm_bindgen(method, getter)]
-    pub fn state(&self) -> Option<ShelfView> {
-        self.get_user(&self.uid)
-    }
-    #[wasm_bindgen(method, setter)]
-    pub fn set_state(&mut self, val: &JsValue) {
-        let json = val.into_serde().unwrap();
-        let shelf = ShelfCRDT::from_json_values(json, 0).unwrap();
-        self.users.get_mut(&self.uid).unwrap().replace(shelf);
-    }
+//     #[wasm_bindgen(js_name = "toString")]
+//     pub fn to_string(&self) -> String {
+//         let mut users = Vec::new();
+//         for (user, shelf) in self.users.iter() {
+//             users.push(format!("{user}: {}", RefCell::borrow(&*shelf)));
+//         }
+//         let users = users.join(", ");
+//         let uid = self.uid.to_string();
 
-    #[wasm_bindgen(js_name = "toString")]
-    pub fn to_string(&self) -> String {
-        let mut users = Vec::new();
-        for (user, shelf) in self.users.iter() {
-            users.push(format!("{user}: {}", RefCell::borrow(&*shelf)));
-        }
-        let users = users.join(", ");
-        let uid = self.uid.as_str();
+//         format!("Awareness(uid:{uid}, users: {{{users}}})")
+//     }
 
-        format!("Awareness(uid:{uid}, users: {{{users}}})")
-    }
+//     // #[wasm_bindgen(js_name = "toJson")]
+//     // pub fn to_json(&self) -> JsValue {
+//     //     let users = js_sys::Object::new();
+//     //     for (user, shelf) in self.users.iter() {
+//     //         let shelf = RefCell::borrow(&*shelf);
+//     //         let json: JSON = JSON::from(shelf.clone());
+//     //         js_sys::Reflect::set(
+//     //             &users,
+//     //             &JsValue::from(user),
+//     //             &JsValue::from_serde(&json).unwrap(),
+//     //         )
+//     //         .unwrap();
+//     //     }
+//     //     let awareness = js_sys::Object::new();
+//     //     js_sys::Reflect::set(&awareness, &JsValue::from("users"), &JsValue::from(users)).unwrap();
+//     //     JsValue::from(awareness)
+//     // }
 
-    // #[wasm_bindgen(js_name = "toJson")]
-    // pub fn to_json(&self) -> JsValue {
-    //     let users = js_sys::Object::new();
-    //     for (user, shelf) in self.users.iter() {
-    //         let shelf = RefCell::borrow(&*shelf);
-    //         let json: JSON = JSON::from(shelf.clone());
-    //         js_sys::Reflect::set(
-    //             &users,
-    //             &JsValue::from(user),
-    //             &JsValue::from_serde(&json).unwrap(),
-    //         )
-    //         .unwrap();
-    //     }
-    //     let awareness = js_sys::Object::new();
-    //     js_sys::Reflect::set(&awareness, &JsValue::from("users"), &JsValue::from(users)).unwrap();
-    //     JsValue::from(awareness)
-    // }
+//     // pub fn encode_state_vector(&) -> js_sys::Uint8Array {
 
-    // pub fn encode_state_vector(&) -> js_sys::Uint8Array {
+//     //     js_sys::Uint8Array::from()
+//     // }
+// }
 
-    //     js_sys::Uint8Array::from()
-    // }
-}
+// #[wasm_bindgen]
+// pub struct ShelfView {
+//     target: Rc<RefCell<ShelfCRDT>>,
+//     path: Vec<String>,
+// }
+// #[wasm_bindgen]
+// impl ShelfView {
+//     fn new(target: Rc<RefCell<ShelfCRDT>>) -> Self {
+//         ShelfView {
+//             target,
+//             path: Vec::new(),
+//         }
+//     }
 
-#[wasm_bindgen]
-pub struct ShelfView {
-    target: Rc<RefCell<ShelfCRDT>>,
-    path: Vec<String>,
-}
-#[wasm_bindgen]
-impl ShelfView {
-    fn new(target: Rc<RefCell<ShelfCRDT>>) -> Self {
-        ShelfView {
-            target,
-            path: Vec::new(),
-        }
-    }
+//     #[wasm_bindgen(js_name = "toString")]
+//     pub fn to_string(&self) -> String {
+//         let shelf = RefCell::borrow(&*self.target);
 
-    #[wasm_bindgen(js_name = "toString")]
-    pub fn to_string(&self) -> String {
-        let shelf = RefCell::borrow(&*self.target);
+//         format!("ShelfView({})", shelf)
+//     }
 
-        format!("ShelfView({})", shelf)
-    }
+//     #[wasm_bindgen(js_name = "toJson")]
+//     pub fn to_json(&self) -> JsValue {
+//         let shelf = self.resolve_path(&self.path);
 
-    #[wasm_bindgen(js_name = "toJson")]
-    pub fn to_json(&self) -> JsValue {
-        let shelf = self.resolve_path(&self.path);
+//         shelf
+//             .map(|shelf| {
+//                 let json: JSON = (shelf.clone()).into();
+//                 JsValue::from_serde(&json).unwrap()
+//             })
+//             .unwrap_or(JsValue::null())
+//     }
 
-        shelf
-            .map(|shelf| {
-                let json: JSON = (shelf.clone()).into();
-                JsValue::from_serde(&json).unwrap()
-            })
-            .unwrap_or(JsValue::null())
-    }
+//     pub fn contents(&self) -> JsValue {
+//         JsValue::from_serde(&RefCell::borrow(&self.target).clone().to_json_values()).unwrap()
+//     }
 
-    pub fn contents(&self) -> JsValue {
-        JsValue::from_serde(&RefCell::borrow(&self.target).clone().to_json_values()).unwrap()
-    }
+//     #[wasm_bindgen(method)]
+//     pub fn get(&self, path: Vec<JsValue>) -> Option<ShelfView> {
+//         let path: Option<Vec<String>> = path.into_iter().map(|key| key.as_string()).collect();
+//         path.map(|path| ShelfView {
+//             path: self.path.iter().cloned().chain(path.into_iter()).collect(),
+//             target: Rc::clone(&self.target),
+//         })
+//     }
+//     #[wasm_bindgen(method)]
+//     pub fn set(&mut self, key: &str, val: &JsValue) -> Result<(), String> {
+//         let shelf = self.resolve_path_mut(&self.path);
+//         if let Some(mut shelf) = shelf {
+//             let val = ShelfContent::from_json_values(JSON::String("foo".to_string()))?;
+//             shelf.set(key.to_string(), val).unwrap();
+//         }
+//         Ok(())
+//     }
+//     #[wasm_bindgen(method, getter)]
+//     pub fn value(&self) -> JsValue {
+//         self.resolve_path(&self.path)
+//             .and_then(|shelf| match shelf.content.as_ref() {
+//                 Some(ShelfContent::Value(val)) => {
+//                     let json: JSON = val.clone().into();
+//                     Some(json)
+//                 }
+//                 Some(ShelfContent::ShelfMap(shelf_map)) => {
+//                     let contents: serde_json::Map<String, JSON> = shelf_map
+//                         .iter()
+//                         .map(|(k, shelf)| (k.clone(), shelf.clone().to_json_values()))
+//                         .collect();
+//                     Some(JSON::Object(contents))
+//                 }
+//                 _ => None,
+//             })
+//             .as_ref()
+//             .and_then(|v| JsValue::from_serde(v).ok())
+//             .unwrap_or(JsValue::null())
+//     }
 
-    #[wasm_bindgen(method)]
-    pub fn get(&self, path: Vec<JsValue>) -> Option<ShelfView> {
-        let path: Option<Vec<String>> = path.into_iter().map(|key| key.as_string()).collect();
-        path.map(|path| ShelfView {
-            path: self.path.iter().cloned().chain(path.into_iter()).collect(),
-            target: Rc::clone(&self.target),
-        })
-    }
-    #[wasm_bindgen(method)]
-    pub fn set(&mut self, key: &str, val: &JsValue) -> Result<(), String> {
-        let shelf = self.resolve_path_mut(&self.path);
-        if let Some(mut shelf) = shelf {
-            let val = ShelfContent::from_json_values(JSON::String("foo".to_string()))?;
-            shelf.set(key.to_string(), val).unwrap();
-        }
-        Ok(())
-    }
-    #[wasm_bindgen(method, getter)]
-    pub fn value(&self) -> JsValue {
-        self.resolve_path(&self.path)
-            .and_then(|shelf| match shelf.content.as_ref() {
-                Some(ShelfContent::Value(val)) => {
-                    let json: JSON = val.clone().into();
-                    Some(json)
-                }
-                Some(ShelfContent::ShelfMap(shelf_map)) => {
-                    let contents: serde_json::Map<String, JSON> = shelf_map
-                        .iter()
-                        .map(|(k, shelf)| (k.clone(), shelf.clone().to_json_values()))
-                        .collect();
-                    Some(JSON::Object(contents))
-                }
-                _ => None,
-            })
-            .as_ref()
-            .and_then(|v| JsValue::from_serde(v).ok())
-            .unwrap_or(JsValue::null())
-    }
+//     #[wasm_bindgen(method, setter)]
+//     pub fn set_value(&mut self, val: &JsValue) -> Result<(), String> {
+//         let json: JSON = val.into_serde().or_else(|e| Err(e.to_string()))?;
+//         let new_content = ShelfContent::from_json_values(json)?;
+//         let mut shelf = self
+//             .resolve_path_mut(&self.path)
+//             .ok_or("Element does not exist".to_string())?;
+//         shelf.set_content(new_content);
+//         Ok(())
+//     }
 
-    #[wasm_bindgen(method, setter)]
-    pub fn set_value(&mut self, val: &JsValue) -> Result<(), String> {
-        let json: JSON = val.into_serde().or_else(|e| Err(e.to_string()))?;
-        let new_content = ShelfContent::from_json_values(json)?;
-        let mut shelf = self
-            .resolve_path_mut(&self.path)
-            .ok_or("Element does not exist".to_string())?;
-        shelf.set_content(new_content);
-        Ok(())
-    }
+//     fn resolve_path(&self, path: &Vec<String>) -> Option<Ref<ShelfCRDT>> {
+//         let target = RefCell::borrow(&*self.target);
+//         let mut cur = target;
+//         for key in path.iter() {
+//             cur = Ref::map(cur, |shelf| shelf.get(&key).unwrap());
+//         }
+//         Some(cur)
+//     }
 
-    fn resolve_path(&self, path: &Vec<String>) -> Option<Ref<ShelfCRDT>> {
-        let target = RefCell::borrow(&*self.target);
-        let mut cur = target;
-        for key in path.iter() {
-            cur = Ref::map(cur, |shelf| shelf.get(&key).unwrap());
-        }
-        Some(cur)
-    }
-
-    fn resolve_path_mut(&self, path: &Vec<String>) -> Option<RefMut<ShelfCRDT>> {
-        let target = self.target.borrow_mut();
-        let mut cur = target;
-        for key in path.iter() {
-            cur = RefMut::map(cur, |shelf| shelf.get_mut(&key).unwrap());
-        }
-        Some(cur)
-    }
-}
+//     fn resolve_path_mut(&self, path: &Vec<String>) -> Option<RefMut<ShelfCRDT>> {
+//         let target = self.target.borrow_mut();
+//         let mut cur = target;
+//         for key in path.iter() {
+//             cur = RefMut::map(cur, |shelf| shelf.get_mut(&key).unwrap());
+//         }
+//         Some(cur)
+//     }
+// }
 
 #[wasm_bindgen]
 pub struct Shelf(ShelfCRDT);
@@ -294,12 +292,12 @@ pub struct Shelf(ShelfCRDT);
 #[wasm_bindgen]
 impl Shelf {
     #[wasm_bindgen(constructor)]
-    pub fn new(content: JsValue) -> Shelf {
+    pub fn new(content: JsValue, client_id: usize) -> Shelf {
         let inner = if content.is_undefined() {
-            ShelfCRDT::default()
+            ShelfCRDT::new(ShelfContent::ShelfMap(HashMap::default()), client_id)
         } else {
             let values = content.into_serde().unwrap_throw();
-            ShelfCRDT::from_json_values(values).unwrap_throw()
+            ShelfCRDT::from_json_values(values, client_id).unwrap_throw()
         };
         Self(inner)
     }
@@ -315,7 +313,7 @@ impl Shelf {
         }
     }
 
-    pub fn set(&mut self, path: Array, contents: JsValue) {
+    pub fn set(&mut self, path: Array, contents: JsValue, client_id: usize) {
         if path.length() < 1 {
             return;
         }
@@ -324,9 +322,9 @@ impl Shelf {
         let shelf = self.resolve_path_mut(&path[..last_idx]);
         if let Some(shelf) = shelf {
             let json = contents.into_serde().unwrap_throw();
-            let content = ShelfContent::from_json_values(json).unwrap_throw();
+            let content = ShelfContent::from_json_values(json, client_id).unwrap_throw();
             let key = path[last_idx].clone();
-            shelf.set(key, content);
+            shelf.set(key, content, client_id);
         }
     }
 
